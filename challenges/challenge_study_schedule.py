76a05820817497dd7d3a76efc7139717144b37f9
Aline <eualine2017@gmail.com>
2022-07-13 18:25:03 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        counter = 0
+        for start, end in permanence_period:
+            if start <= target_time <= end:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
