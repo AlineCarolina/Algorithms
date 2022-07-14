04a34d7bf8e352aea7fe72a2b7bc88db992915a0
Aline <eualine2017@gmail.com>
2022-07-14 18:04:36 -0300

    fix: all

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
+def anagram(str):
+    anagram_dict = {}
+    for char in str.lower():
+        if char in anagram_dict:
+            anagram_dict[char] += 1
+        else:
+            anagram_dict[char] = 1
+    return anagram_dict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    if anagram(first_string) != anagram(second_string):
+        return False
+    return True
