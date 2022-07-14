63e5e8536898821c2cd9af4b19e7e3a78956c376
Aline <eualine2017@gmail.com>
2022-07-14 17:25:19 -0300

    Requisito 3

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,6 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    # https://stackoverflow.com/questions/509211/understanding-slicing
+    if len(word) > 0 and word == word[::1]:
+        return True
+    else:
+        return False
