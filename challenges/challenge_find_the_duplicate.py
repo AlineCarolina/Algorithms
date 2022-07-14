881863a0f42f5eeb22f2210af74f2642020eb3ec
Aline <eualine2017@gmail.com>
2022-07-14 19:12:30 -0300

    Requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) < 2:
+        return False
+
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+
+        if nums.count(num) > 1:
+            return num
+            
+    return False
